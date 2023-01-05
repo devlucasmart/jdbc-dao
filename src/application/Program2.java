7257f94a33b37774fbe824e5f53aaab423c80b00
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-01-05 08:54:11 -0300

    [ADD] - mudanças no projeto e em sua estrutura

---
+++ src/application/Program2.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		/*programa secundario para testar o dao de depatment*/
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author lucas
+ *
+ */
+module jdbc {
+	requires java.sql;
+}
